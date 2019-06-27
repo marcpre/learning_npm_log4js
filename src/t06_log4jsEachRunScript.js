0119d2fc8bbf58cc04e2b01c89230ee339c0b50d
marcpre <marcus.presich@gmail.com>
2019-06-27 16:05:47 +0200

    create log file for each run

---
+++ src/t06_log4jsEachRunScript.js
@@ -0,0 +1,19 @@
+
+// require log4js
+const log4js = require('log4js');
+
+// Logger configuration
+log4js.configure("./config/t06_log4js-config.json");
+
+const logger = log4js.getLogger();
+
+logger.level = 'info';
+logger.info('Beginning program execution...');
+
+// Log a message
+logger.trace('Trace, log4js!');
+logger.debug('Debug, log4js!');
+logger.info('Hello, log4js!');
+logger.warn('Heads up, log4js!');
+logger.error('Danger, log4js!');
+logger.fatal('Fatal, log4js!');
